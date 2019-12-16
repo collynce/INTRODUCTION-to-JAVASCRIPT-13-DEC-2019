c0bae94331adde8745543e1f685c2119596a02d4
Collins Kiprotich <collinsrono02@gmail.com>
2019-12-16 16:48:46 +0300

    Initial Commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@
 
 //Closure
 function myValues() {
-    var num1 = 10;
-    var num2 = 15;
+    let num1 = 10;
+    let num2 = 15;
     function addNumbers() {
         return num1+num2;
     }
     return addNumbers;
 }
-var addFunction =  myValues();
+let addFunction =  myValues();
 console.log(addFunction());
 
 //Function Prototype
@@ -21,7 +21,7 @@ function Cars(carName, price , type) {
 Cars.prototype.name = function () {
     return this.carName+ " " +this.price+ " "+ this.type;
 };
-var myCar = new Cars("Volvo", "$20,000", "SUV");
+let myCar = new Cars("Volvo", "$20,000", "SUV");
 console.log(myCar.name());
 
 //Inheritance
@@ -33,14 +33,14 @@ function Store(storeName, storeLocation, carName, price, type) {
 Store.prototype = new Cars();
 Store.prototype.name = Store;
 
-var newStore = new Store('GM Motors', 'Nairobi', 'Toyota', '200,000','SUV');
+let newStore = new Store('GM Motors', 'Nairobi', 'Toyota', '200,000','SUV');
 console.log(newStore);
 
 
 
 function greetings() {
-    var myDate = new Date();
-    var myHours = myDate.getHours();
+    let myDate = new Date();
+    let myHours = myDate.getHours();
 
     if (myHours >= 0 && myHours <12 ){
         console.log('Good Morning');
